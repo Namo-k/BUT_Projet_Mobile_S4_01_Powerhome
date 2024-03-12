312f78e77e1af628b185c13b8f72d0252ecfa1fc
Namo-k <namodacane@etu.u-paris.fr>
2024-03-12 04:05:29 +0100

    Drawer menu front

---
+++ app/src/main/java/fr/iut/projet_mobile_s4_01_powerhome/TestActivity.java
@@ -0,0 +1,36 @@
+package fr.iut.projet_mobile_s4_01_powerhome;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.widget.TextView;
+import android.widget.Toolbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.google.android.material.navigation.NavigationView;
+
+import fr.iut.projet_mobile_s4_01_powerhome.R;
+
+public class TestActivity extends AppCompatActivity {
+
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    Toolbar toolbar;
+    Menu menu;
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.test);
+
+        /*---------------------Hooks------------------------*/
+        /*drawerLayout=findViewById(R.id.drawer_layout);
+        navigationView=findViewById(R.id.nav_view);
+        textView=findViewById(R.id.textView);
+        toolbar=findViewById(R.id.toolbar);*/
+    }
+
+}
