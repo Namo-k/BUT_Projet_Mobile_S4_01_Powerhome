83062ff8d502c4fab0fae176dc471bf3a10f3b35
Alexandre <128932398+alexandrecarou@users.noreply.github.com>
2024-03-26 16:01:55 +0100

    update mode sombre/clair

--- app/src/main/java/fr/iut/projet_mobile_s4_01_powerhome/app/user/PreferenceFragment.java
+++ app/src/main/java/fr/iut/projet_mobile_s4_01_powerhome/app/user/PreferenceFragment.java
@@ -1,23 +1,35 @@
 package fr.iut.projet_mobile_s4_01_powerhome.app.user;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 import java.util.Locale;
 import android.widget.Button;
 
+import androidx.appcompat.widget.SwitchCompat;
 import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
 import fr.iut.projet_mobile_s4_01_powerhome.R;
+import fr.iut.projet_mobile_s4_01_powerhome.app.equipement.EquipementAddActivity;
 
 public class PreferenceFragment extends Fragment {
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
     private String mParam1;
     private String mParam2;
+    SwitchCompat switchMode;
+    boolean nightMode;
+    SharedPreferences sharedPreferences;
+    SharedPreferences.Editor editor;
+
 
     public PreferenceFragment() {
         // Required empty public constructor
@@ -44,7 +56,36 @@ public class PreferenceFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_preference, container, false);
+        View view = inflater.inflate(R.layout.fragment_preference, container, false);
 
+
+        switchMode = view.findViewById(R.id.switchMode);
+        switchMode.setThumbResource(R.drawable.thumb);
+        switchMode.setTrackResource(R.drawable.track);
+
+        sharedPreferences = requireContext().getSharedPreferences("MODE", Context.MODE_PRIVATE);
+        nightMode = sharedPreferences.getBoolean("nightMode", false);
+
+        if (nightMode) {
+            switchMode.setChecked(true);
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+        }
+        switchMode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (nightMode) {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                    editor = sharedPreferences.edit();
+                    editor.putBoolean("nightMode", false);
+                }
+               else {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                    editor = sharedPreferences.edit();
+                    editor.putBoolean("nightMode", true);
+                }
+               editor.apply();
+            }
+        });
+        return view;
     }
 }
